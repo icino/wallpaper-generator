f056061e70a27fdd1c744bcfb8eaf31b828acc93
icino <annisaawhy@gmail.com>
2020-07-23 23:39:48 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const btn = document.querySelector("button")
+const wallpaper = document.querySelector(".wallpaper")
+const loader = document.querySelector(".loader")
+const url = "https://source.unsplash.com/random"
+
+function giveNewImageUrl() {
+    return url + "/" + Math.floor(Math.random() * 100)
+}
+
+wallpaper.addEventListener('load', function() {
+    loader.style.setProperty('display', 'none')
+    wallpaper.style.setProperty('display', 'block')
+
+})
+
+btn.addEventListener('click', function() {
+    loader.style.setProperty('display', 'block')
+    wallpaper.style.setProperty('display', 'none')
+    wallpaper.src = giveNewImageUrl()
+})
